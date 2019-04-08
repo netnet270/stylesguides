964a4855c9056c1b4accf908d748f2335eb7460e
nguyen.thi.net <FRAMGIA\nguyen.thi.net@b121787.framgia.com>
2019-04-08 10:26:21 +0700

    fix chart

---
+++ source/js/main.js
@@ -0,0 +1,12 @@
+//=require jquery/dist/jquery.min.js
+//=require bootstrap/dist/js/bootstrap.min.js
+//=require _custom.js
+
+$(document).ready(function() {
+  $(".js-toggle-icon").each(function (index, element) {
+    $(this).click(function (e) { 
+      e.preventDefault();
+        $(this).toggleClass("show");
+    });
+  });
+});
